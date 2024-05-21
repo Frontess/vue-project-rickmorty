296bcfc8e6613e822f3b6e1c3b0810263edb91cd
Marina <presto.m@mail.ru>
2024-05-21 20:01:57 +0300

    rick-morty

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+
+  <Input />
+  <Characters />
+  <Pagination />
+</template>
+  
+<script>
+import Characters from './components/Characters.vue'
+import Input from './components/Input.vue'
+import Pagination from './components/Pagination.vue';
+
+export default {
+  components: { Characters, Input, Pagination },
+ 
+  // mounted() {
+  //   this.applyFilter(); // Вызов метода при загрузке страницы
+  // },
+  // methods: {
+  //   async applyFilter() {
+  //     try {
+  //       const response = await fetch('https://rickandmortyapi.com/api/character');
+  //       const data = await response.json();
+  //       this.characters = data.results;
+  //     } catch (error) {
+  //       console.error('Ошибка при получении данных о персонажах:', error);
+  //     }
+  //   }
+    
+  // }
+};
+
+</script>
+
+<style scoped>
+
+</style> 
---
+++ src/components/Pagination.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="buttons">
+      <button class="btn" @click="prevPage">⬅ Back</button>
+      <button class="btn" @click="nextPage">Next ➡</button>
+    </div>
+  </template>
+  
+  <script>
+  import { computed } from "vue";
+  import { useStore } from "vuex";
+  
+  export default {
+    setup() {
+      const store = useStore();
+  
+      const pageNext = computed(() => {
+        return store.state.next;
+      });
+  
+      const pagePrev = computed(() => {
+        return store.state.prev;
+      });
+  
+      const prevPage = () => {
+        if (pagePrev.value) {
+          store.dispatch("getCharacters", pagePrev.value);
+        }
+      };
+  
+      const nextPage = () => {
+        if (pageNext.value) {
+          store.dispatch("getCharacters", pageNext.value);
+        }
+      };
+  
+      return {
+        prevPage,
+        nextPage,
+      };
+    },
+  };
+  </script>
+  
